f823d2de9072bf061286bdb143e956ee152e316c
afzsilva <afzsilva@gmail.com>
2021-08-22 09:17:39 -0300

    Cenário contador aula 10

--- src/test/java/AprenderCucumber.java
+++ src/test/java/AprenderCucumber.java
@@ -2,6 +2,9 @@ import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.Então;
+import cucumber.api.java.pt.Quando;
 
 public class AprenderCucumber {
 
@@ -22,6 +25,27 @@ public class AprenderCucumber {
 	public void aEspecificaçãoDeveFinalizarComSucesso() throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
 	    //throw new PendingException();
+	}
+	
+	
+	int contador = 0;
+	@Dado("^que o valor do contador é (\\d+)$")
+	public void queOValorDoContadorÉ(int arg1) throws Throwable {	    
+	    this.contador = arg1;
+	}
+
+	@Quando("^quando eu incrementar em (\\d+)$")
+	public void quandoEuIncrementarEm(int arg1) throws Throwable {
+	    this.contador = contador + arg1;
+	}
+
+	@Então("^o valor do contador é (\\d+)$")
+	public void oValorDoContadorÉ(int arg1) throws Throwable {	    
+		//this.contador = arg1;
+		System.out.println(arg1);
+		System.out.println(contador);
+		System.out.println(contador == arg1);
+		
 	}
 	
 }
--- src/test/java/aprender_cucumber.feature
+++ src/test/java/aprender_cucumber.feature
@@ -1,9 +1,15 @@
-Feature: Aprender Cucumber
+# language: pt
+Funcionalidade: Aprender Cucumber
 	Como um aluno
 	Eu quero aprender a usar cucumber
 	Para que eu possa automatizar critérios de aceitação
 
-Scenario:
-	Given que criei o arquivo corretamente
-	When executá-lo
-	Then a especificação deve finalizar com sucesso
+Cenario: Deve executar uma especificação
+	Dado que criei o arquivo corretamente
+	Quando executá-lo
+	Então a especificação deve finalizar com sucesso
+	
+Cenario: Deve incremnetar um contador
+	Dado que o valor do contador é 15
+	Quando quando eu incrementar em 5
+	Então o valor do contador é 18
