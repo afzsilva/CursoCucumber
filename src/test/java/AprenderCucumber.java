b442609e2a43ce883da907dfa7a66df20c566877
afzsilva <afzsilva@gmail.com>
2021-08-22 18:26:17 -0300

    Parte 3 do desafio

--- src/test/java/AprenderCucumber.java
+++ src/test/java/AprenderCucumber.java
@@ -128,20 +128,20 @@ public class AprenderCucumber {
 	
 
 //CENARIO 1
-@Dado("^que o ticket( especial)? é A.(\\d+)$")
-public void queOTicketÉAF(String tipo,int arg1) throws Throwable {
+@Dado("^que o ticket( especial)? é (A.\\d{3})$")
+public void queOTicketÉAF(String tipo, String arg1) throws Throwable {
 }
 
 @Dado("^que o valor da passagem é R\\$ (\\d+),(\\d+)$")
 public void queOValorDaPassagemÉR$(int arg1, int arg2) throws Throwable {
 }
 
-@Dado("^que o nome do passageiro é \"(.*)\"$")
+@Dado("^que o nome do passageiro é \"(.{5,20})\"$")
 public void queONomeDoPassageiroÉ(String arg1) throws Throwable {
 }
 
-@Dado("^que o telefone do passageiro é (\\d+)-(\\d+)$")
-public void queOTelefoneDoPassageiroÉ(int arg1, int arg2) throws Throwable {
+@Dado("^que o telefone do passageiro é (9\\d{3}-\\d{4})$")
+public void queOTelefoneDoPassageiroÉ(String telefone) throws Throwable {
 }
 
 @Quando("^criar os steps$")
