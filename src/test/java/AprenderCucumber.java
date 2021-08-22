e744dc033c203b8a210aba744775b114ba030f5c
afzsilva <afzsilva@gmail.com>
2021-08-22 18:41:34 -0300

    Converter para data declarado

--- src/test/java/AprenderCucumber.java
+++ src/test/java/AprenderCucumber.java
@@ -5,7 +5,6 @@ import java.util.Date;
 
 import org.junit.Assert;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -151,13 +150,6 @@ public void criarOsSteps() throws Throwable {
 @Então("^o teste vai funcionar$")
 public void oTesteVaiFuncionar() throws Throwable {
 }
-
-
-
-//CENARIO 2
-
-
-
 
 
 }// class
---
+++ src/test/java/DateConverter.java
@@ -0,0 +1,22 @@
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import cucumber.api.Transformer;
+
+public class DateConverter extends Transformer<Date>{
+
+	@Override
+	public Date transform(String arg0) {
+		DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+		try {
+			Date dateRetornado = format.parse(arg0);
+			return dateRetornado;
+		} catch (ParseException e) {			
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+}
