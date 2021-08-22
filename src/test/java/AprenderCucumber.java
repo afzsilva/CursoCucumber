2db058193e94fda81ecce22e701021cb692813c5
afzsilva <afzsilva@gmail.com>
2021-08-22 16:26:53 -0300

    termino da aula 12 e começo da aula 14

--- src/test/java/AprenderCucumber.java
+++ src/test/java/AprenderCucumber.java
@@ -1,5 +1,11 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import org.junit.Assert;
 
+import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -12,43 +18,179 @@ public class AprenderCucumber {
 	@Given("^que criei o arquivo corretamente$")
 	public void queCrieiOArquivoCorretamente() throws Throwable {
 		System.out.println("Faça alguma coisa");
-	    // Write code here that turns the phrase above into concrete actions
-	    //throw new PendingException();
+		// Write code here that turns the phrase above into concrete actions
+		// throw new PendingException();
 	}
 
 	@When("^executá-lo$")
 	public void executáLo() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    //throw new PendingException();
+		// Write code here that turns the phrase above into concrete actions
+		// throw new PendingException();
 	}
 
 	@Then("^a especificação deve finalizar com sucesso$")
 	public void aEspecificaçãoDeveFinalizarComSucesso() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    //throw new PendingException();
+		// Write code here that turns the phrase above into concrete actions
+		// throw new PendingException();
 	}
-	
-	
+
 	int contador = 0;
+
 	@Dado("^que o valor do contador é (\\d+)$")
-	public void queOValorDoContadorÉ(int arg1) throws Throwable {	    
-	    this.contador = arg1;
+	public void queOValorDoContadorÉ(int arg1) throws Throwable {
+		this.contador = arg1;
 	}
 
 	@Quando("^quando eu incrementar em (\\d+)$")
 	public void quandoEuIncrementarEm(int arg1) throws Throwable {
-	    this.contador = contador + arg1;
+		this.contador = contador + arg1;
 	}
 
 	@Então("^o valor do contador é (\\d+)$")
-	public void oValorDoContadorÉ(int arg1) throws Throwable {	    
-		//this.contador = arg1;
+	public void oValorDoContadorÉ(int arg1) throws Throwable {
+		// this.contador = arg1;
 //		System.out.println(arg1);
 //		System.out.println(contador);
 //		System.out.println(contador == arg1);
 		Assert.assertTrue(contador == arg1);
-		//throw new RuntimeException();
+		// throw new RuntimeException();
+
+	}
+
+//*************************DATA
+
+	Date entrega = new Date();
+
+	@Dado("^que a entrega é dia (\\d+)/(\\d+)/(\\d+)$")
+	public void queAEntregaÉDia(int dia, int mes, int ano) throws Throwable {
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.DAY_OF_MONTH, dia);
+		cal.set(Calendar.MONTH, mes - 1);
+		cal.set(Calendar.YEAR, ano);
+		entrega = cal.getTime();
+	}
+
+	@Quando("^quando a entrega atrasar em (\\d+) dias$")
+	public void quandoAEntregaAtrasarEmDias(int arg1) throws Throwable {
+		Calendar cal = Calendar.getInstance();// instância
+		cal.setTime(entrega);// transforma de entrega em calendar
+		cal.add(Calendar.DAY_OF_MONTH, arg1); // adiciona dias
+		entrega = cal.getTime();
+
+	}
+
+	@Então("^a entrega sera efetuada em (\\d{2}\\/\\d{2}\\/\\d{4})$")
+	public void aEntregaSeraEfetuadaEm(String data) throws Throwable {
+		DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+		String dataFormatada = format.format(entrega);
+		Assert.assertEquals(data, dataFormatada);
+	}
+	
+	
+	@Dado("^que o prazo é dia (\\d+)/(\\d+)/(\\d+)$")
+	public void queOPrazoÉDia(int dia, int mes, int ano) throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		//throw new PendingException();
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.DAY_OF_MONTH, dia);
+		cal.set(Calendar.MONTH, mes - 1);
+		cal.set(Calendar.YEAR, ano);
+		entrega = cal.getTime();
+	}
+
+	@Quando("^a entrega atrasar em (\\d+) (dia|dias|mes|meses)$")
+	public void aEntregaAtrasarEmMeses(int arg1, String tempo) throws Throwable {
+		Calendar cal = Calendar.getInstance();// instância
+		cal.setTime(entrega);// transforma de entrega em calendar
+		if(tempo.equals("dias")) {
+			cal.add(Calendar.DAY_OF_MONTH, arg1);
+		}
+		
+		if(tempo.equals("meses")) {
+			cal.add(Calendar.MONTH, arg1); // adiciona dias			
+		}
+		
+		entrega = cal.getTime();
+		
+	}
+
+	@Então("^a entrega é efetuada em (\\d{2}/\\d{2}/\\d{4})$")
+	public void aEntregaÉEfetuadaEm(String data) throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		//throw new PendingException();
+		DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+		String dataFormatada = format.format(entrega);
+		Assert.assertEquals(data, dataFormatada);
 		
 	}
 	
+//	******************Desafio
+	
+
+@Dado("^que o ticket é AF(\\d+)$")
+public void queOTicketÉAF(int arg1) throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
 }
+
+@Dado("^que o valor da passagem é R\\$ (\\d+),(\\d+)$")
+public void queOValorDaPassagemÉR$(int arg1, int arg2) throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@Dado("^que o nome do passageiro é \"([^\"]*)\"$")
+public void queONomeDoPassageiroÉ(String arg1) throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@Dado("^que o telefone do passageiro é (\\d+)-(\\d+)$")
+public void queOTelefoneDoPassageiroÉ(int arg1, int arg2) throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@Quando("^criar os steps$")
+public void criarOsSteps() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@Então("^o teste vai funcionar$")
+public void oTesteVaiFuncionar() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@Dado("^que o ticket é AB(\\d+)$")
+public void queOTicketÉAB(int arg1) throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@Dado("^que o ticket especial é AB(\\d+)$")
+public void queOTicketEspecialÉAB(int arg1) throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@Dado("^que o ticket é CD(\\d+)$")
+public void queOTicketÉCD(int arg1) throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@Dado("^que o ticket é AG(\\d+)$")
+public void queOTicketÉAG(int arg1) throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@Dado("^que o valor da passagem é R\\$ (\\d+)\\.(\\d+),(\\d+)$")
+public void queOValorDaPassagemÉR$(int arg1, int arg2, int arg3) throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+}// class
--- src/test/java/aprender_cucumber.feature
+++ src/test/java/aprender_cucumber.feature
@@ -13,3 +13,38 @@ Cenario: Deve incremnetar um contador
 	Dado que o valor do contador é 15
 	Quando quando eu incrementar em 3
 	Então o valor do contador é 18
+
+Cenario: Deve calcular atraso na entrega
+	Dado que a entrega é dia 05/04/2018
+	Quando quando a entrega atrasar em 2 dias
+	Então a entrega sera efetuada em 07/04/2018
+	
+Cenario: Deve calcular atraso no prazo de entrega da china
+	Dado que o prazo é dia 05/04/2018
+	Quando a entrega atrasar em 2 meses
+	Então a entrega é efetuada em 05/06/2018	
+
+#desafio
+Cenário: Deve criar steps genéricos para estes passos
+    Dado que o ticket é AF345
+    Dado que o valor da passagem é R$ 230,45
+    Dado que o nome do passageiro é "Fulano da Silva"
+    Dado que o telefone do passageiro é 9999-9999
+    Quando criar os steps
+    Então o teste vai funcionar
+
+Cenário: Deve reaproveitar os steps "Dado" do cenário anterior
+    Dado que o ticket é AB167
+    Dado que o ticket especial é AB167
+    Dado que o valor da passagem é R$ 1120,23
+    Dado que o nome do passageiro é "Cicrano de Oliveira"
+    Dado que o telefone do passageiro é 9888-8888
+
+Cenário: Deve negar todos os steps "Dado" dos cenários anteriores
+    Dado que o ticket é CD123
+    Dado que o ticket é AG1234
+    Dado que o valor da passagem é R$ 1.1345,56
+    Dado que o nome do passageiro é "Beltrano Souza Matos de Alcântara Azevedo"
+    Dado que o telefone do passageiro é 1234-5678
+    Dado que o telefone do passageiro é 999-2223 
+	   
